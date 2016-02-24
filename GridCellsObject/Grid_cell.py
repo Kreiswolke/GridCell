f94c9d3a7bc26325e98e4313922c5e65154139a2
Kreiswolke <oliver_eberle@web.de>
2016-02-24 17:15:24 +0000

    Update Grid_cell.py

--- GridCellsObject/Grid_cell.py
+++ GridCellsObject/Grid_cell.py
@@ -7,6 +7,7 @@ Created on Wed Jan 14 11:02:14 2015
 
 import numpy as np
 
+# Defining the GridCell Object
 class GridCell:
     
     def __init__(self):
@@ -43,44 +44,38 @@ class GridCell:
         self.gainvalues=[]
         self.thresholdvalues=[]
         
+    # Defining Heaviside function    
     def heaviside(self,the_x):
         if the_x > 0:
             the_result = 1
-       # elif the_x == 0:
-        #    the_result = 0.5
         else:
             the_result = 0
-    
         return the_result
-    
-        
-        
+ 
+    # Keeping track of the Rat's current position and head direction angle
     def Rat_Location(self, params):
         RatPos=self.params['RatPos']
         speed=self.params['speed']*self.params['dt']
         sidelength=self.params['sidelength']
         angle=self.params['angle']
         
-        angle = np.random.normal(angle, 0.2)        
+        angle=np.random.normal(angle, 0.2)        
         RatPos = RatPos + ([np.sin(angle), np.cos(angle)] / (np.linalg.norm([np.sin(angle), np.cos(angle)]))) * speed
         self.params['RatPos']=RatPos
         
         while (abs(RatPos[0]) > sidelength / 2.0) or (abs(RatPos[1]) > sidelength / 2.0):
-            RatPos=self.params['RatPos']
+            RatPos = self.params['RatPos']
             angle = np.random.normal(angle, 0.2)
             RatPos = RatPos + ([np.sin(angle), np.cos(angle)] / (np.linalg.norm([np.sin(angle), np.cos(angle)]))) * speed
         
 
         return RatPos, angle
         
-    def get_Rat_Location(self):
-        return self.params['RatPos']            
-        
+ 
+    # Implement numerical integration (Euler method)        
     def eulerstep(self,f_func, startvalue, params):
         return startvalue + f_func(startvalue, self.params) * params['dt']
-    
-    def set_Grid_Locations(self, Locations):
-        self.params['GridPos']=Locations
+
 
     def firingrate(self,  params):
         GridPos=self.params['GridPos']
@@ -92,21 +87,16 @@ class GridCell:
             Rates[i] = np.exp(-((np.linalg.norm(RatPos - GridPos[i,:])) ** 2) / (2 * (sig_p ** 2)))
         return Rates
         
-    def get_firingrate(self):
-        return self.params['Rates']
         
-        
-        #Adaptation Dynamics
+    # Adaptation Dynamics
     def rminusdot(self, rminus, params):
         tauminus=self.params['tauminus']
-        #h=self.hfunc(self.params)
         h=self.params['hi']
         return (h - rminus) / tauminus
         
     def rplusdot(self, rplus, params):
         tauplus=params['tauplus']
         rminus=params['rminus']
-       # h=params['h']
         
         rminus=self.eulerstep(self.rminusdot, rminus, self.params)
         self.params['rminus']=rminus
@@ -120,50 +110,31 @@ class GridCell:
         
         return np.sin(np.pi * k * (t ** 2))
         
-    def get_rplus(self):
-        #return self.params['rplus']
-        return self.rplusgrid
         
-    def get_rminus(self):
-        return self.params['rminus']
-
-        
-    #Initialize Weights
+    # Initialize Weights
     def randweights(self):
         eta=0.1
-        #eta=0.2        
         weights=np.zeros(225)
         for i in range(len(weights)):
             weights[i] = (1 - eta) + eta * (np.random.uniform(0, 1))
         weights = weights / (np.linalg.norm(weights))
-    #    self.params['weights']=weights
+        # self.params['weights']=weights
         return np.transpose(weights)
         
-    def set_initial_weights(self):
-        self.weights=np.zeros((225,100))
-        for i in range(100):
-            self.weights[:,i]=self.randweights() 
-        return self.weights
-        
-  #  def get_weights(self):
-   #     return self.params['weights']
-        
-    #Feed forward input
+
+    # Feed forward input
     def h_i(self):
         h=np.zeros(100)
         for i in range(100):
             h[i]=np.dot(self.weights[:,i], self.params['Rates'])
         return h
         
-    def get_h_i(self):
-        return self.params['hi']
         
-    #Adaptation Dynamics
+    # Adaptation Dynamics
     def OutputRate(self):
         rplus=self.rplusgrid
         threshold=self.threshold
         gain=self.gain
-        
         rout=np.zeros(100)
         
         for i in range(len(rout)):
@@ -171,9 +142,8 @@ class GridCell:
         
         return rout
         
-    def get_OutputRate(self):
-        return self.rout
-      
+
+    # Implementing the learning dynamics of the fatigue parameters
     def fatigue_dynamics(self, params):
         limit=100
         N_out=self.params['N_out']
@@ -185,14 +155,8 @@ class GridCell:
         a = np.sum(r_out) / float(N_out)
         s = ((np.sum(r_out))**2) / (float(N_out)*(np.sum(r_out**2)))        
         self.params['a'], self.params['s'] = a, s
-       # print 's outer loop', s
-        #print 'a outer loop', a
-
-        #gain=gain+0.1*gain*(s-0.3)
-        #threshold=threshold+0.01*(a-0.1)
         self.gain=gain
         self.threshold=threshold
-        
         
         
         if self.t <= limit*self.params['dt']:
@@ -205,8 +169,6 @@ class GridCell:
                 
                 r_out=self.OutputRate()
                 a = np.sum(r_out) / float(N_out)
-              #  A.append(a)
-               # print A
                 self.params['a']=a
                
 
@@ -223,7 +185,6 @@ class GridCell:
                     
                     r_out=self.OutputRate()
               
-                    
                 
                 if  (abs(s-0.3)/0.3)  > 0.1:
                     gain=self.gain
@@ -236,6 +197,8 @@ class GridCell:
 
                 self.params['s']=s
                 self.params['a']=a
+                
+                # Setting time limits for i > 200
                 i+=1
                 if i > 100:
                     if i%500 == 0:
@@ -243,13 +206,10 @@ class GridCell:
                 if i > 200:
                     break
 
-
-        
         return  gain, threshold
         
     def learning_dynamics(self):
         eps=0.005
-        #eps=0.02
 
         weights=self.weights
         rout_mean=self.rout_mean
@@ -261,8 +221,6 @@ class GridCell:
 
         return weights
         
-    def get_weights(self):
-        return self.weights
         
     def estimate_mean_rates(self):
         rout_mean=self.rout_mean
@@ -275,42 +233,75 @@ class GridCell:
         Rates_mean=Rates_mean + eta*(Rates-Rates_mean)
         
         return rout_mean, Rates_mean
+    
+     
+    # This is the main function to drive learning, all relevant variables are updated in every timestep         
+    def update(self):
+        # Move forward in discrete time
+        self.t+= self.params['dt']
+        
+        #Update Rat Loation
+        self.params['RatPos'], self.params['angle']= self.Rat_Location(self.params)
+        
+        #Update Input firing
+        self.params['Rates']=self.firingrate(self.params)
+    
+        # Update parameters
+        self.rplusgrid=self.eulerstep(self.rplusdot,self.rplusgrid,self.params)
+        self.params['hi']=self.h_i()
+        
+        # Update output rate
+        self.rout=self.OutputRate()
+        
+        # Update fatigue dynamics
+        self.gain, self.threshold = self.fatigue_dynamics(self.params)
+        
+        # Update estimated mean firing rates
+        self.rout_mean, self.Rates_mean = self.estimate_mean_rates()
+
+        # Update weights
+        self.weights=self.learning_dynamics()
+        
+        
+    ######################################### Getter and setter methods###########################################
+    def get_Rat_Location(self):
+        return self.params['RatPos']   
+        
+    def get_firingrate(self):
+        return self.params['Rates']
+        
+    def get_rplus(self):
+        return self.rplusgrid
+        
+    def get_rminus(self):
+        return self.params['rminus']
+        
+    def get_OutputRate(self):
+        return self.rout
+        
+    def get_h_i(self):
+        return self.params['hi']
+        
+    def get_weights(self):
+        return self.weights
         
     def get_mean_rates(self):
         return self.rout_mean, self.Rates_mean
-        
-#    def get_gain_values(self):
-#        return self.gainvalues, self.threshold
         
     def get_gain_threshold(self):
         return self.gain, self.threshold
         
     def get_a_s(self):
         return  self.params['a'], self.params['s']
-             
-    def update(self):
-        self.t+= self.params['dt']
-
-        self.params['RatPos'], self.params['angle']= self.Rat_Location(self.params)
-        self.params['Rates']=self.firingrate(self.params)
-    
-        self.rplusgrid=self.eulerstep(self.rplusdot,self.rplusgrid,self.params)
+           
+    def set_Grid_Locations(self, Locations):
+        self.params['GridPos'] = Locations
         
-        self.params['hi']=self.h_i()
-        
-        
-        self.rout=self.OutputRate()
-        
-
-        #Update fatigue dynamics
-        self.gain, self.threshold = self.fatigue_dynamics(self.params)
-       # self.params['gain'], self.params['threshold'] = 0.1, 0.3
-        
-         #Update estimated mean firing rates
-        self.rout_mean, self.Rates_mean = self.estimate_mean_rates()
-
-        #Update weights
-        self.weights=self.learning_dynamics()
+    def set_initial_weights(self):
+        self.weights=np.zeros((225,100))
+        for i in range(100):
+            self.weights[:,i]=self.randweights() 
+        return self.weights
         
 
     
